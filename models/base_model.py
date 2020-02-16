5d6da80f16495457afdb5a8dbaef6ec0922f29df
jorgechauxjr <chauxjorge@hotmail.com>
2020-02-16 23:14:24 +0000

    Add amenity, base_model, city, __init__, place, review, state and user.py

---
+++ models/base_model.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""Module for Base class
+Contains the Base class for the AirBnB clone console.
+"""
+
+import uuid
+from datetime import datetime
+from models import storage
+
+
+class BaseModel:
+
+    """Class for base model"""
+    def __init__(self, *args, **kwargs):
+        """Initialization"""
+        if kwargs is not None and kwargs != {}:
+            for key in kwargs:
+                if key == "created_at":
+                    self.__dict__["created_at"] = datetime.strptime(
+                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(
+                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[key] = kwargs[key]
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
+
+    def __str__(self):
+        """str format"""
+        return "[{}] ({}) {}".\
+            format(type(self).__name__, self.id, self.to_dict())
+
+    def save(self):
+        """updates."""
+        self.updated_at = datetime.now()
+        storage.save()
+
+    def to_dict(self):
+        """dictionary format"""
+        new_dict = dict(self.__dict__)
+        new_dict["__class__"] = type(self).__name__
+        new_dict["created_at"] = new_dict["created_at"].isoformat()
+        new_dict["updated_at"] = new_dict["updated_at"].isoformat()
+        return new_dict
